1ef5a7eb73590a4a53cc1791ee11040052a9515a
yj21 <33304837+yj21@users.noreply.github.com>
2019-12-15 23:58:42 -0800

    init commit

---
+++ build/precache-manifest.78bd6fa5b91605e01b1212f7d7050c87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93ac341022fa6a9ec3fc2ee1cab39342",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab3f24a261c9a38013f2",
+    "url": "/static/css/main.324c2a1d.chunk.css"
+  },
+  {
+    "revision": "23abcbfefca95e9ed58c",
+    "url": "/static/js/2.7beeb27e.chunk.js"
+  },
+  {
+    "revision": "ab3f24a261c9a38013f2",
+    "url": "/static/js/main.ab7498e0.chunk.js"
+  },
+  {
+    "revision": "d43e78c8bbb21d4f0e7e",
+    "url": "/static/js/runtime-main.c35eacba.js"
+  }
+]);
